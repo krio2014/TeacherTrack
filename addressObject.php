391eb47b3f47680144782f9300c51f29ba4ec1a4
Adam Evans <adamevans385@msn.com>
2016-12-29 22:11:14 +0000

    tidy addressObject.php
    removed unused commented areas and cleaned up the script to make it
    easier to read.

--- addressObject.php
+++ addressObject.php
@@ -3,66 +3,46 @@
 class address{
     
     public $people;
-    
-    
+        
     //Constructs a new address object
-    public function __construct(){
-    
-        //Makes the people variable global;
-        global $people;
-        /*
-		//Diagnostic Code
-        echo '<i>|-----Address constructed-----|</i>';
-		*/
+    public function __construct()
+    {
+//        global $people;
     }
 
 	/* adds a person object provided by $aPerson
-    *  pre condition - $aPerson should be a person object that has already been initialised 
+    *   pre condition - $aPerson should be a person object 
+    *   that has already been initialised 
 	*/
-    public function addPerson($aPerson){
+    public function addPerson($aPerson)
+    {
         global $people;
-        
         $this->people[] = $aPerson;
-
         $length = count($people)-1;
-
-//        echo '<i>|---\'addressObject.php\': '.$aPerson->getName().' added at index ['.$length.']---|</i>';
-//        var_dump($people);
-        return $this->people;
-        
+        return $this->people;    
     }	
 	
 	/*
     * returns a person object from the people array at the provided index
     */
-	function getPerson($index){
-//        global $people;
-		
-//        echo '<p><i>|----\'addressObject.php\':Person object returned at ['.$index.']----|</i><p/>';
+	function getPerson($index)
+    {
         return $this->people[$index];
     }
 	
-    
-	public function getPeople(){
+    /*
+    *   returns the people array
+    */
+	public function getPeople()
+    {
 		return $this->people;
 	}
-	
-
-	
+		
     /*
 	*	Returns the name of a person in the people array at the $index provided
 	*/
-    function getPersonName($index){ 
-        global $people;
-//        echo '<p><i>|----Person object name returned ['.$index.']----|</i><p/>';
-        return $this->people[$index]->getName();
-        
+    function getPersonName($index)
+    {    
+        return $this->people[$index]->getName();   
     }
-    
-    
-    
-    
-    
-    
-    
 }
