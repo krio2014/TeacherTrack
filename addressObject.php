842d680cb243e30af8608ff395339bcd22e60091
krio2014 <adamevans385@msn.com>
2016-12-29 10:36:15 +0000

    tidying up comments
    tidied up comments for addressObject to make more readable

--- addressObject.php
+++ addressObject.php
@@ -16,8 +16,9 @@ class address{
 		*/
     }
 
-	//adds a person object provided by $aPerson
-    //pre condition - $aPerson should be a person object that has already been initialised 
+	/* adds a person object provided by $aPerson
+    *  pre condition - $aPerson should be a person object that has already been initialised 
+	*/
     public function addPerson($aPerson){
         global $people;
         
@@ -33,15 +34,18 @@ class address{
 	
 	
 	
-	
-    // returns a person object from the people array at the provided index
-    function getPerson($index){
+	/*
+    * returns a person object from the people array at the provided index
+    */
+	function getPerson($index){
         global $people;
         echo '<p><i>|----Person object returned at ['.$index.']----|</i><p/>';
         return $people[$index];
     }
     
-    //Returns the name of a person in the people array at the $index provided
+    /*
+	*	Returns the name of a person in the people array at the $index provided
+	*/
     function getPersonName($index){ 
         global $people;
         echo '<p><i>|----Person object name returned ['.$index.']----|</i><p/>';
@@ -49,7 +53,9 @@ class address{
         
     }
     
-    // Returns the people array
+    /* 
+	* Returns the people array
+	*/
     public function getPeople(){
         echo '<p><i>|----People array returned----|</i><p/>';
        global $people;
