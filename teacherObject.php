27b396d0a21f5a40fc5623dc34d417c0c120f322
Adam Evans <adamevans385@msn.com>
2017-01-05 21:40:17 +0000

    added functions addClass and removeClass
    - added functions addClass and removeClass
    - also updated all the $class variable to be called $lesson

--- teacherObject.php
+++ teacherObject.php
@@ -2,20 +2,34 @@
 
 class teacher extends person{
  
-    private $classList;
+    private $lessonList;
     
  public function __construct(){
-     $this->classList = array();
+     $this->lessonList = array();
 }
 
-    public function addClass(){
+    public function addLesson($aLesson){
         
+        try
+        {
+            $this->lessonList[] = $aLesson;
+        }
+        catch(Exception $e)
+        {
+            echo "Error: ".$e;
+        }
     }
     
-    public function removeClass(){
+    public function removeLesson($aLesson){
         
+        $key = array_search($aSubject,$this->lessonList); 
+        
+        if(in_array($aLesson, $this->lessonList))
+        {            
+            unset($this->lessonList[$key]);
+            echo 'Succesfully removed '.$aLesson.' from '.$this->fullName.'<p/>';
+        }
     }
-    
     
 }
 
