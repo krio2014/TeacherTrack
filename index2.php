c712395410dfb92003a9f0a062e189c7519cfdb9
krio2014 <adamevans385@msn.com>
2017-01-15 12:50:45 +0000

    remove Var_dump and changed who manages lessons (#1)
    - Deleted all var_dumps() across all live files
    - added subjectList variable to lessonObject.php
    - removed errolledSubjects variable from studentObject.php

--- index2.php
+++ index2.php
@@ -1,25 +1,27 @@
 <?Php
 require 'studentObject.php';
 require_once 'subjectObject.php';
+require_once 'teacherObject.php';
 //require 'addressObject.php';
 //require 'addressObject.php';
 require_once 'personObject.php';
+require_once 'lessonObject.php';
 echo '<pre>';
 //$person1 = new person("adam","03/06/1987","328 worlemoor", "bs247ah","01275 400 650","07896983691","a@a.com");
-$student = new student("adam","03/06/1987","123 the road", "bs247ah","01275 400 652","07896983692","b@b.com");
+//$student = new student("adam","03/06/1987","123 the road", "bs247ah","01275 400 652","07896983692","b@b.com");
 
-$subject = new subject();
+//$subject = new subject();
 
-$var1 = $subject->getSubjectList();
+//$var1 = $subject->getSubjectList();
 
 //var_dump($var1);
 
-$student->addSubject("Maths",$var1);
-$student->addSubject("Maths1",$var1);
-$student->addSubject("History",$var1);
-$student->removeSubject("History");
+//$student->addSubject("Maths",$var1);
+//$student->addSubject("Maths1",$var1);
+//$student->addSubject("History",$var1);
+//$student->removeSubject("History");
 
-$es = $student->getEnrolledSubjects();
+//$es = $student->getEnrolledSubjects();
 //var_dump($es);
 
 
@@ -39,6 +41,18 @@ $es = $student->getEnrolledSubjects();
 //var_dump($subjectList);
 
 //$student->addSubject("Maths",$subjectList);
+
+//TESTING lesson Object
+
+$teacher = new teacher("adam","03/06/1987","123 the road", "bs247ah","01275 400 652","07896983692","b@b.com");
+
+//var_dump($teacher);
+
+$lesson = new lesson();
+
+$lesson->addLesson("maths","03/06/1987", $teacher);
+
+//var_dump($lesson);
 echo'</pre>';
 
 ?>
--- lessonObject.php
+++ lessonObject.php
@@ -2,10 +2,63 @@
 
 class lesson{
 
-public function __construct(){
-
+    //Lesson list contains a list of all schedules lessons and their participants.
+    private $lessonList;
+    private $subjectList;
+    
+    
+    public function __construct(){
+    
+    $this->lessonList = array();
+    
+    
 }
-
+    
+   
+    
+    /*
+    *   returns the subjectList as an array
+    *
+    */
+    function getSubjects()
+    {
+        return $this->subjectlist;
+    }
+    
+    /*
+    *   adds a new subjec to the subjectList Array
+    *
+    */
+    function addSubject()
+    {
+        
+    }
+    
+    
+    /*
+    *   returns the lesson list as an array
+    *
+    */
+    function getLessons()
+    {
+        return $this->lessonList;
+    }
+    
+     function addLesson($aSubject,$aDate,$aTeacher)
+    {
+        try{
+            $this->lessonList[] = array($aSubject, $aDate,$aTeacher, null); 
+            
+              
+        }
+        catch(Exception $e)
+        {
+            echo "error: ".$e;
+        }
+        
+  
+    }
+    
 }
 
 
