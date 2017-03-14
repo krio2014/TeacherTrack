b1f1a467b0e8c8a3309295f52cbe27749aaee28f
krio2014 <adamevans385@msn.com>
2017-03-14 14:47:37 +0000

    Added ui elements
    Added ui elements

--- UI/index.php
+++ UI/index.php
@@ -1,23 +1,35 @@
 <?php
 require ('../co.php');
-
-echo '<html>';
-echo '<head>';
-echo '</head>';
-echo '<body>';
+include ('header.html');
+//echo '<html>';
+//echo '<head>';
+//echo '</head>';
+//echo '<body>';
 
 echo '
-
+<div class="col-2">Hi</div>
+<div class="col-2">HI</div>
+  <div class="card-block">
 	<form action="/action_page.php">
-	<label for="fname">First name: </label>
-	<input type="text" name="fname"><br>
-	<label>
-  Last name: <input type="text" name="lname"><br>
-  <input type="submit" value="Submit">
-</form>';
+	 <div class="form-group">
+		<label for="fname">First name: </label>
+		<input class="form-control" type="text" name="fname"><br>
+		<label>
+  		Last name: <input class="form-control" type="text" name="lname"><br>
+  		<input type="submit" value="Submit">
+  
+  	</div>
+</form>
+</div>
+</div>
 
-echo '</body>';
-echo '</html>';
 
+
+';
+
+//echo '</body>';
+//echo '</html>';
+
+include('../footer.html');
 
 ?>
